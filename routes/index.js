58770d161ed3f5a418a09a34d31f49a83a2003ae
Eric <sktsoi@connect.ust.hk>
2024-04-20 16:49:24 +0800

    Initial Author Route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) =>{
-    res.render('index', { layout: 'layouts/layout' })
+    res.render('index')
 })
 
 module.exports = router
